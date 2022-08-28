e308120df1913a477fb381b7cf712aad346a360b
pokavv <okaps@hanmail.net>
2022-08-28 23:47:30 +0900

    220828 baekjoon

---
+++ 6810_220828.py
@@ -0,0 +1,5 @@
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+
+print("The 1-3-sum is", 91 + n1 + n2 * 3 + n3)
---
+++ 8370_220828.py
@@ -0,0 +1,3 @@
+num1, key1, num2, key2 = map(int, input().split())
+
+print((num1 * key1) + (num2 * key2))
