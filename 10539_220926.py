183e60264da90b92b107f23f293588edbd34dab8
pokavv <okaps@hanmail.net>
2022-09-26 23:59:43 +0900

    220926 baekjoon

---
+++ 10539_220926.py
@@ -0,0 +1,10 @@
+num = int(input())
+num_arr = list(map(int, input().split()))
+arr = []
+arr.append(num_arr[0])
+
+for i in range(1, num):
+    arr.append((num_arr[i] * (i + 1)) - sum(arr))
+
+for j in arr:
+    print(j, end='')
