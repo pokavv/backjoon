5cc4bd0aececa5d02ce824c2dd349665e3e147d7
pokavv <okaps@hanmail.net>
2022-10-05 23:39:20 +0900

    221005 baekjoon

---
+++ 25024_221003.py
@@ -0,0 +1,12 @@
+for i in range(int(input())):
+    a,b=map(int,input().split())
+    print("Yes" if 0 <= a <= 23 and 0 <= b <= 59 else "No", end=" ")
+    
+    if a == 2:
+        print("Yes" if 0 <= b <= 29 else "No")
+    elif a in [1, 3, 5, 7, 8, 10, 12]:
+        print("Yes" if b <= 30 else "No")
+    elif a in [4, 6, 9, 11]:
+        print("Yes" if b <= 31 else "No")
+    else:
+        print("No")
