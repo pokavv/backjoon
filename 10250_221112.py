c509003ce5e53cd77668dc47e026c27687afcc2b
pokavv <okaps@hanmail.net>
2022-11-12 23:58:44 +0900

    221112 baekjoon

---
+++ 10250_221112.py
@@ -0,0 +1,13 @@
+for i in range(int(input())):
+    h, w, n = map(int, input().split())
+    floor = 0
+    ho = 0
+    
+    if n % h == 0:
+        floor = h * 100
+        ho = n // h
+    else:
+        floor = (n % h) * 100
+        ho = 1 + n // h
+    
+    print(floor + ho)
