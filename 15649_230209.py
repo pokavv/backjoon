45a2fab87df1923867a7c05a2a0dcd163b533e90
pokavv <okaps@hanmail.net>
2023-02-11 00:39:19 +0900

    230210 baekjoon

--- 15649_230209.py
+++ 15649_230209.py
@@ -1,3 +1,27 @@
+def dfs():
+    if len(nums) == b:
+        print(' '.join(map(str, nums)))
+        return
+    for i in range(1, a+1):
+        if visited[i]:
+            continue
+        
+        visited[i] = True
+        nums.append(i)
+        dfs()
+        nums.pop()
+        
+        # print(nums)
+        # print(visited)
+        visited[i] = False
+
+a, b = map(int, input().split())
+nums = []
+visited = [False] * (a + 1)
+
+dfs()
+
+'''
 import sys
 import itertools
 input = sys.stdin.readline
@@ -11,3 +35,4 @@ for i in arr:
     for j in i:
         print(j, end=' ')
     print()
+'''
