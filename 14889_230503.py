a6169acc8dc133fdf6a9dd58cc58f4ddf1d6197d
pokavv <okaps@hanmail.net>
2023-05-04 22:57:38 +0900

    230504 baekjoon

--- 14889_230503.py
+++ 14889_230503.py
@@ -2,31 +2,33 @@ import sys
 input = sys.stdin.readline
 
 def dfs(depth, idx):
-    global n, res
+    global min_diff
     
     if depth == n // 2:
         p1, p2 = 0, 0
+        
         for i in range(n):
             for j in range(n):
-                if visit[i] and visit[j]:
-                    p1 += s[i][j]
-                elif not visit[i] and not visit[j]:
-                    p2 += s[i][j]
+                if visited[i] and visited[j]:
+                    p1 += graph[i][j]
+                elif not visited[i] and not visited[j]:
+                    p2 += graph[i][j]
         
-        res = min(res, abs(p1 - p2))
+        min_diff = min(min_diff, abs(p1 - p2))
         
         return
-
+    
     for i in range(idx, n):
-        if not visit[i]:
-            visit[i] == 1
+        if not visited[i]:
+            visited[i] = True
             dfs(depth + 1, i + 1)
-            visit[i] = 0
+            visited[i] = False
 
 n = int(input())
-s = [list(map(int, input().split())) for _ in range(n)]
-visit = [0] * n
-res = 0
+
+visited = [False for i in range(n)]
+graph = [list(map(int, input().split())) for _ in range(n)]
+min_diff = int(1e9) # 1e9 : 가능한 최댓값이 10억 미만이라면 무한(INF) 값으로 1e9 이용가능
 
 dfs(0, 0)
-print(res)
+print(min_diff)
