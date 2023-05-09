2dbde99b611497718ce24bd99efb28d9921f7c78
pokavv <okaps@hanmail.net>
2023-05-09 23:41:46 +0900

    230509 baekjoon

---
+++ 1149_230509.py
@@ -0,0 +1,19 @@
+# 1번 집의 색은 2번 집의 색과 같지 않아야 한다.
+# N번 집의 색은 N-1번 집의 색과 같지 않아야 한다.
+# i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다.
+
+import sys
+input = sys.stdin.readline
+
+house = int(input())
+dp = []
+
+for i in range(house):
+    dp.append(list(map(int, input().split())))
+
+for i in range(1, len(dp)):
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + dp[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + dp[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + dp[i][2]
+
+print(min(dp[house-1][0], dp[house-1][1], dp[house-1][2]))
