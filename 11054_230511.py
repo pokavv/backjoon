11d7764ec5315621153efc2275b57f492d05e33d
pokavv <okaps@hanmail.net>
2023-05-11 23:58:05 +0900

    230511 baekjoon

--- 11054_230511.py
+++ 11054_230511.py
@@ -8,11 +8,13 @@ increase_arr = [1 for _ in range(n)]
 decrease_arr = [1 for _ in range(n)]
 res = [0 for _ in range(n)]
 
+# increase
 for i in range(n):
     for j in range(i):
         if arr[i] > arr[j] and increase_arr[i] < increase_arr[j] + 1:
             increase_arr[i] = increase_arr[j] + 1
 
+# decrease
 for i in range(n - 1, -1, -1):
     for j in range(i + 1, n):
         if arr[i] > arr[j] and decrease_arr[i] < decrease_arr[j] + 1:
