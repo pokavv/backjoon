6253c53192a3e180767109100985812f546ddebb
pokavv <okaps@hanmail.net>
2023-05-28 22:25:56 +0900

    230528 baekjoon

--- 12865_230528.py
+++ 12865_230528.py
@@ -4,17 +4,18 @@ input = sys.stdin.readline
 n, k = map(int, input().rstrip().split())
 bag = [(0, 0)]
 dp = [[0] * (k + 1) for _ in range(n + 1)]
+
 for _ in range(n):
     w, v = map(int, input().rstrip().split())
     bag.append((w, v))
 
 for i in range(1, n + 1):
-    w, v = bag[i]
-    
     for j in range(1, k + 1):
-        if j - w <= 0:
-            dp[i][j] = dp[i-1][j]
-        else:
+        w, v = bag[i]
+        
+        if j >= w:
             dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + v)
+        else:
+            dp[i][j] = dp[i - 1][j]
 
-print(dp[n][k])
+print(dp[i][k])
